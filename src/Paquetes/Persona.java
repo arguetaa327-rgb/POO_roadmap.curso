f41c47a00daff0ad381a463e9fe5ffacbf66f336
Alondra Argueta <arguetaa327@gmail.com>
2026-06-05 12:02:44 -0600

    Semana 3 paquetes y Encapsulamiento

---
+++ src/Paquetes/Persona.java
@@ -0,0 +1,17 @@
+package Paquetes;
+
+public class Persona {
+
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
---
+++ src/Paquetes/UsandoPaquetes.java
@@ -0,0 +1,13 @@
+package Paquetes;
+
+public class UsandoPaquetes {
+
+    public static void ejecutar() {
+
+        System.out.println("\n=== EJEMPLO DE PAQUETES ===");
+
+        Persona persona1 = new Persona("Yuliana", 20);
+
+        persona1.mostrarInformacion();
+    }
+}
